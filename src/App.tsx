ae78f6b66ebf9b150b686b34a1a8334a1a327ee2
trungpc2108 <trungpcph39068@fpt.edu.vn>
2024-09-30 17:19:00 +0700

    update apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/layout";
 
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Layout from './components/layout'
-
-import Page from './components/page'
-
-import CartPage from './components/cartPage'
-import Long from './components/long'
-import Homepages from './components/homepages'
-
-
-
+import Homepages from "./components/homepages";
+import Header from "./components/header";
 
 function App() {
-
   return (
     <>
-      <Routes >
-        <Route path='' element={<Layout />}>
-        <Route path=' admin' element={<Homepages />}/>
+      <Routes>
+        <Route path="" element={<Layout />}>
+          <Route path=" admin" element={<Homepages />} />
+          <Route path="login" element={<Header />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
