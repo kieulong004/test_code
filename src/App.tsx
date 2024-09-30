bb7bfd32c85fde72fe496b89cf483727f024df96
trungpc2108 <trungpcph39068@fpt.edu.vn>
2024-09-30 17:11:02 +0700

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/layout";
 
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Layout from './components/layout'
+import Page from "./components/page";
 
-import Page from './components/page'
-
-import CartPage from './components/cartPage'
-import Long from './components/long'
-
-
-
+import CartPage from "./components/cartPage";
+import Long from "./components/long";
 
 function App() {
-
   return (
     <>
-      <Routes >
-        <Route path='' element={<Layout />}>
-
-          <Route path='page' element={<Page />}/>
-          <Route path='page' element={<CartPage />}/>
-          <Route path='  ' element={<Long />}/>
-          <Route path='cart' element={<CartPage />}/>
-
-
-
+      <Routes>
+        <Route path="" element={<Layout />}>
+          <Route path="okela" element={<Page />} />
+          <Route path="page" element={<Page />} />
+          <Route path="page" element={<CartPage />} />
+          <Route path="  " element={<Long />} />
+          <Route path="cart" element={<CartPage />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
